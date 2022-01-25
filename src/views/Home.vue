58be039ab60841d78c974c92181eaaaefadf4930
Mehmet <unal.m1991@gmail.com>
2022-01-26 00:16:04 +0300

    start html

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>SAMPLE PAGE</div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+module.exports = {
+  resolve: {
+    extensions: ["*", ".js", ".vue", ".json"],
+    alias: {
+      "@": path.resolve("src"),
+    },
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        loader: 'css-loader',
+        options: {
+          url: true,
+        },
+      },
+    ],
+  },
+};
